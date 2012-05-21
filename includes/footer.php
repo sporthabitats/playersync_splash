c37c2268e3cd13fec706fbb151e42d5e13007355
Scott Giovanni <sgiovanni@sporthabitats.com>
2012-05-21 12:12:11 -0700

    more work on the splash

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+    <div class="container">
+    	<div class="footer">
+    		
+    		<div class="container">
+    			<div class="footer-links">
+    				<a href="/about.html">About</a>
+    				<a href="http://blog.playersync.com">Blog</a>
+    				<a href="#">Community</a>
+    			</div>
+    			<div class="copyright">&#169; Playersync 2012</div>
+    		</div>
+   	 	</div>
+    </div>
+
+
+    <script src="js/jquery/jquery-1.7.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script> 
+    <script src="js/home.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/respond.src.js" type="text/javascript" charset="utf-8"></script>
+    
+        <script type="text/javascript"> 
+        $(document).ready(function(){ 
+                $('#app-screens').carousel({ 
+                        interval: 5000, 
+                }); 
+        }); 
+        </script> 
+
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Playersync - Scott</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    
+
+ 
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+
+	<div id="ps-navbar" class="navbar">
+        <div id="nav-container" class="container">
+          <a id="logo" href="/"><img src="img/playersync_200w.png" /></a>
+        </div>
+    </div>
